f429c27f2c640abf543499c71fdd6f31d99d84ac
CIPHERLAB\david.chou <yubo0826@gmail.com>
2024-09-24 18:05:01 +0800

    Optimizing mouse fluency

--- src/components/ImgCompSlider.vue
+++ src/components/ImgCompSlider.vue
@@ -1,149 +1,344 @@
 <template>
-  <div ref="containerRef" class="img-comp-container">
-    <input class="move-area" type="text" ref="moveRef"
-      @mousedown="slideDown" 
-      @mousemove="slideMove"
-      @keydown="keyDownSlide"
-    >
-    <div class="img-comp-img">
-      <img :src="imageUrl1" :width="width" :height="height">
+  <!-- https://codepen.io/Pushedskydiver/pen/GevRaK -->
+  <section class="image-comparison" data-component="image-comparison-slider">
+    <div class="image-comparison__slider-wrapper">
+      <label for="image-comparison-range" class="image-comparison__label">Move image comparison slider</label>
+      <input ref="sliderRef" type="range" min="0" max="100" value="50" class="image-comparison__range" id="image-compare-range">
+
+      <div ref="overlayRef" class="image-comparison__image-wrapper  image-comparison__image-wrapper--overlay" data-image-comparison-overlay="">
+        <figure class="image-comparison__figure image-comparison__figure--overlay">
+          <picture class="image-comparison__picture">
+            <source media="(max-width: 40em)" srcset="https://images.unsplash.com/photo-1566702580807-95611c919b47?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&h=400&q=80">
+            <source media="(min-width: 40.0625em) and (max-width: 48em)" srcset="https://images.unsplash.com/photo-1566702580807-95611c919b47?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&h=600&q=80">
+            <img src="https://images.unsplash.com/photo-1566702580807-95611c919b47?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&h=1000&q=80" alt="Mojave desert in the sun" class="image-comparison__image">
+          </picture>
+
+          <figcaption class="image-comparison__caption image-comparison__caption--before">
+            <span class="image-comparison__caption-body">Before Label</span>
+          </figcaption>
+        </figure>
+      </div>
+
+      <div class="image-comparison__slider" data-image-comparison-slider="">
+        <span ref="thumbRef" class="image-comparison__thumb" data-image-comparison-thumb="">
+          <svg class="image-comparison__thumb-icon" xmlns="http://www.w3.org/2000/svg" width="18" height="10" viewBox="0 0 18 10" fill="currentColor">
+            <path class="image-comparison__thumb-icon--left" d="M12.121 4.703V.488c0-.302.384-.454.609-.24l4.42 4.214a.33.33 0 0 1 0 .481l-4.42 4.214c-.225.215-.609.063-.609-.24V4.703z"></path>
+            <path class="image-comparison__thumb-icon--right" d="M5.879 4.703V.488c0-.302-.384-.454-.609-.24L.85 4.462a.33.33 0 0 0 0 .481l4.42 4.214c.225.215.609.063.609-.24V4.703z"></path>
+          </svg>
+        </span>
+      </div>
+
+      <div class="image-comparison__image-wrapper">
+        <figure class="image-comparison__figure">
+          <picture class="image-comparison__picture">
+            <source media="(max-width: 40em)" srcset="https://images.unsplash.com/photo-1554110397-9bac083977c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&height=400&q=80">
+            <source media="(min-width: 40.0625em) and (max-width: 48em)" srcset="https://images.unsplash.com/photo-1554110397-9bac083977c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&height=600&q=80">
+            <img src="https://images.unsplash.com/photo-1554110397-9bac083977c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&height=1000&q=80" alt="Mojave desert in the dark" class="image-comparison__image">
+          </picture>
+
+          <figcaption class="image-comparison__caption image-comparison__caption--after">
+            <span class="image-comparison__caption-body">After Label</span>
+          </figcaption>
+        </figure>
+      </div>
     </div>
-    <!-- <div ref="sliderRef" class="img-comp-slider move"></div> -->
-    <img ref="sliderRef" src="@/assets/expand-arrows.png" class="img-comp-slider move" alt="">
-    <div ref="imgOverlay" class="img-comp-img img-comp-overlay move">
-      <img :src="imageUrl2" :width="width" :height="height">
-    </div>
-  </div>
+  </section>
 </template>
 <script setup lang="ts">
 import { ref, defineProps, onMounted } from 'vue'
 
-const props = defineProps({
-  img: {
-    type: Array,
-    required: true
-  },
-  width: {
-    type: Number,
-    required: false,
-    default: 600
-  },
-  height: {
-    type: Number,
-    required: false,
-    default: 400
+// const props = defineProps({
+//   img: {
+//     type: Array,
+//     required: true
+//   },
+//   width: {
+//     type: Number,
+//     required: false,
+//     default: 600
+//   },
+//   height: {
+//     type: Number,
+//     required: false,
+//     default: 400
+//   }
+// })
+
+const sliderRef = ref<HTMLElement>()
+const thumbRef = ref<HTMLElement>()
+const overlayRef = ref<HTMLElement>()
+
+function setSliderstate(e: Event, element: HTMLElement) {
+  if (!sliderRef.value) return 
+  if (e.type === 'input') {
+    sliderRef.value.classList.add('image-comparison__range--active')
+    return;
   }
-})
 
-const imageUrl1 = new URL(props.img[0] as string, import.meta.url).href
-const imageUrl2 = new URL(props.img[1] as string, import.meta.url).href
-const imgOverlay = ref<HTMLElement | null>(null)
-const sliderRef = ref<HTMLElement | null>(null)
-const containerRef = ref<HTMLElement | null>(null)
+  sliderRef.value.classList.remove('image-comparison__range--active')
+  element.removeEventListener('mousemove', moveSliderThumb)
+}
 
-var slider: HTMLElement
-var img: HTMLElement
-var container: HTMLElement
-var clicked: boolean
+function moveSliderThumb(e: MouseEvent) {
+  const sliderRange = sliderRef.value
+  const thumb = thumbRef.value
+  if (!sliderRange || !thumb) return
+
+  let position = e.layerY - 20
+  if (e.layerY <= sliderRange.offsetTop) {
+    position = -20
+  }
+
+  if (e.layerY >= sliderRange.offsetHeight) {
+    position = sliderRange.offsetHeight - 20
+  }
+
+  thumb.style.top = `${position}px`
+}
+
+function moveSliderRange(e: Event, element: HTMLElement) {
+  const sliderRange = sliderRef.value
+  const overlay = overlayRef.value
+  const target = e.target as HTMLInputElement
+  if (!sliderRange || !overlay) return
+
+  const value = target.value
+
+  sliderRange.style.left = `${value}%`
+  overlay.style.width = `${value}%`
+
+  element.addEventListener('mousemove', moveSliderThumb)
+  setSliderstate(e, element)
+}
+
+function init(element: HTMLElement) {
+  const sliderRange = sliderRef.value
+  if (!sliderRange) return
+
+  // 移動端: 判斷瀏覽器是否支持touch事件
+  if ('ontouchstart' in window === false) {
+    sliderRange.addEventListener('mouseup', (e: MouseEvent) => setSliderstate(e, element))
+    sliderRange.addEventListener('mousedown', moveSliderThumb)
+  }
+  
+  sliderRange.addEventListener('input', (e: Event) => moveSliderRange(e, element))
+  sliderRange.addEventListener('change', (e: Event) => moveSliderRange(e, element))
+}
 
 onMounted(() => {
-  slider = sliderRef.value as HTMLElement
-  img = imgOverlay.value as HTMLElement
-  container = containerRef.value as HTMLElement
-  slider.style.left = (img.offsetWidth / 2) - (slider.offsetWidth / 2) + "px"
-  slider.style.top = (img.offsetHeight / 2) - (slider.offsetHeight / 2) + "px"
-  img.style.width = (img.offsetWidth / 2) + "px"
-  window.addEventListener('mouseup', slideLeave)
-})
-
-function slideDown(e: MouseEvent) {
-  console.log('slideDown')
-  // e.preventDefault()
-  clicked = true
-  let mouseX = e.offsetX
-  slider.style.left = mouseX - (slider.offsetWidth / 2) + "px"
-  img.style.width = mouseX + 'px'
-}
-
-function slideMove(e: MouseEvent) {
-  console.log('slideMove');
-  if (!clicked) return false
-  let mouseX = e.offsetX
-  console.log(mouseX);
-  
-  if (mouseX < 0) mouseX = 0
-  slider.style.left = mouseX - (slider.offsetWidth / 2) + "px"
-  img.style.width = mouseX + 'px'
-}
-
-function keyDownSlide(e: KeyboardEvent) {
-  console.log('keyDownSlide')
-  let width = parseInt(img.style.width)
-  let offset: number
-  if (e.key === 'ArrowLeft') {
-    offset = width < 20 ? 0 : width - 20
-    slider.style.left =  offset - slider.offsetWidth / 2 + 'px'
-    img.style.width = offset + 'px'
-    return
+  if (sliderRef.value) {
+    init(sliderRef.value)
   }
-  offset = width + 20 > container.offsetWidth ? container.offsetWidth : width + 20
-  slider.style.left =  offset - slider.offsetWidth / 2 + 'px'
-  img.style.width = offset + 'px'
-}
-
-function slideLeave(e: MouseEvent) {
-  console.log('slideLeave')
-  clicked = false
-}
-
-
+})
 </script>
-<style>
-* {box-sizing: border-box;}
 
-.move-area {
-  position: absolute;
-  z-index: 10;
-  width: inherit;
-  height: inherit;
-  opacity: 0;
-  cursor: default;
+<style scoped>
+body {
+  font-family: "Helvetica", sans-serif;
 }
 
-.img-comp-container {
+.image-comparison {
+  max-width: 48.063em;
+  margin-right: auto;
+  margin-left: auto;
+}
+
+.image-comparison__slider-wrapper {
   position: relative;
-  height: 400px; /*should be the same height as the images*/
-  width: 600px;
 }
 
-.img-comp-img {
+.image-comparison__label {
+  font-size: 0;
+  line-height: 0;
+}
+
+.image-comparison__label,
+.image-comparison__range {
   position: absolute;
-  width: auto;
-  height: auto;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  background-color: transparent;
+  border: none;
+  -webkit-appearance: none;
+     -moz-appearance: none;
+          appearance: none;
+  outline: none;
+  cursor: ew-resize;
+  z-index: 20;
+}
+
+@media (hover) {
+  .image-comparison__range:hover ~ .image-comparison__slider .image-comparison__thumb {
+    transform: scale(1.2);
+  }
+}
+
+.image-comparison .image-comparison__slider-wrapper .image-comparison__range:active ~ .image-comparison__slider .image-comparison__thumb,
+.image-comparison .image-comparison__slider-wrapper .image-comparison__range:focus ~ .image-comparison__slider .image-comparison__thumb,
+.image-comparison .image-comparison__slider-wrapper .image-comparison__range--active ~ .image-comparison__slider .image-comparison__thumb {
+  transform: scale(0.8);
+  background-color: rgba(0, 97, 127, 0.5);
+}
+
+.image-comparison__image-wrapper--overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: calc(50% + 1px);
+  height: 100%;
   overflow: hidden;
 }
 
-.img-comp-img img {
-  display: block;
-  vertical-align: middle;
+.image-comparison__figure {
+  margin: 0;
+}
+.image-comparison__figure::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #f2f2f2;
+}
+.image-comparison__figure:not(.image-comparison__figure--overlay) {
+  position: relative;
+  padding-top: 66.666666667%;
 }
 
-.img-comp-slider {
+.image-comparison__image {
   position: absolute;
-  z-index: 9;
-  /* cursor: ew-resize; */
-  /*set the appearance of the slider:*/
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  -o-object-fit: cover;
+     object-fit: cover;
+  -o-object-position: 0 50%;
+     object-position: 0 50%;
+  overflow: hidden;
+}
+.image-comparison__figure--overlay .image-comparison__image {
+  z-index: 1;
+}
+
+.image-comparison__caption {
+  position: absolute;
+  bottom: 12px;
+  min-width: -webkit-max-content;
+  min-width: -moz-max-content;
+  min-width: max-content;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  color: #fff;
+  font-weight: bold;
+  text-transform: uppercase;
+}
+@media screen and (max-width: 40.063em) {
+  .image-comparison__caption {
+    font-size: 12px;
+  }
+}
+
+.image-comparison__caption--before {
+  left: 12px;
+  z-index: 2;
+}
+
+.image-comparison__caption--after {
+  right: 12px;
+  text-align: right;
+}
+
+.image-comparison__caption-body {
+  max-width: 40vmin;
+  padding: 6px 12px;
+  background-color: rgba(0, 0, 0, 0.55);
+}
+
+.image-comparison__slider {
+  position: absolute;
+  top: 0;
+  left: 50%;
+  width: 2px;
+  height: 100%;
+  background-color: #fff;
+  transition: background-color 0.3s ease-in-out;
+  z-index: 10;
+}
+.image-comparison__range--active ~ .image-comparison__slider {
+  background-color: rgba(255, 255, 255, 0);
+}
+
+.image-comparison__thumb {
+  position: absolute;
+  top: calc(50% - 20px);
+  left: calc(50% - 20px);
   width: 40px;
   height: 40px;
-  /* background-color: #2196F3; */
-  opacity: 0.7;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #0091df;
+  color: #fff;
   border-radius: 50%;
+  box-shadow: 0 0 22px 0 rgba(0, 0, 0, 0.5);
+  transform-origin: center;
+  transition: transform 0.3s ease-in-out, background-color 0.3s ease-in-out;
 }
 
-.move {
-  transition: all .1s ease-out;
+.image-comparison__range::-webkit-slider-runnable-track {
+  width: 40px;
+  height: 40px;
+  opacity: 0;
 }
 
+.image-comparison__range::-moz-range-thumb {
+  width: 40px;
+  height: 40px;
+  opacity: 0;
+}
+
+.image-comparison__range::-webkit-slider-thumb {
+  width: 40px;
+  height: 40px;
+  opacity: 0;
+}
+
+.image-comparison__range::-ms-fill-lower {
+  background-color: transparent;
+}
+
+.image-comparison__range::-ms-track {
+  position: relative;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border: none;
+  margin: 0;
+  padding: 0;
+  background-color: transparent;
+  color: transparent;
+  outline: none;
+  cursor: col-resize;
+}
+
+.image-comparison__range::-ms-thumb {
+  width: 0.5%;
+  height: 100%;
+  opacity: 0;
+}
+
+.image-comparison__range::-ms-tooltip {
+  display: none;
+}
 
 </style>
 
